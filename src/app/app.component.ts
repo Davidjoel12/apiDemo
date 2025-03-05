99c6258b54bed4457c16e712f8a162e8259fa88c
Davidjoel12 <167281247+Davidjoel12@users.noreply.github.com>
2025-03-05 02:47:33 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { PokemonService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonListComponent, PokemonDetailComponent, CommonModule],
+  imports: [RouterOutlet, PokemonListComponent, PokemonDetailComponent, CommonModule, HttpClient],
+  providers: [PokemonService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http'; 
+import { PokemonService } from './service/api.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    PokemonService,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideClientHydration(withEventReplay())
